62b990ad7916188909c6090dd7b57265b8ee435b
Jordan Matthiesen <1333029+jmatthiesen@users.noreply.github.com>
2023-11-15 11:27:08 -0800

    Updated to show file extensions.

--- setupenv.ps1
+++ setupenv.ps1
@@ -1,1 +1,14 @@
-[System.Environment]::SetEnvironmentVariable("VS_UNIFIED_SETTINGS_PROFILE","c:\src\personal-config\visualstudio-settings\settings.json", "User")
+[System.Environment]::SetEnvironmentVariable("VS_UNIFIED_SETTINGS_PROFILE","c:\src\personal-config\visual-studio-settings\", "User")
+
+# Show extensions in Windows Explorer
+$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$propertyName = "HideFileExt"
+$value = "0"
+
+# Create the registry key if it doesn't exist
+if (!(Test-Path $path)) {
+    New-Item -Path $path -Force | Out-Null
+}
+
+# Set the property
+Set-ItemProperty -Path $path -Name $propertyName -Value $value
