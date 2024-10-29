5c8682960413c7a6f9f128cef481a025e132d8c3
Jordan Matthiesen <1333029+jmatthiesen@users.noreply.github.com>
2024-10-29 16:32:40 -0700

    Update setupenv.ps1

--- setupenv.ps1
+++ setupenv.ps1
@@ -1,4 +1,4 @@
-[System.Environment]::SetEnvironmentVariable("VS_UNIFIED_SETTINGS_PROFILE","c:\src\personal-config\visual-studio-settings\", "User")
+[System.Environment]::SetEnvironmentVariable("VS_UNIFIED_SETTINGS_PROFILE","${WinGetConfigRoot}\..\visual-studio-settings\", "User")
 
 # Show extensions in Windows Explorer
 $path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
@@ -10,3 +10,12 @@ if (!(Test-Path $path)) {
 }
 
 Set-ItemProperty -Path $path -Name $propertyName -Value $value
+
+# Set default font for Terminal
+winhome=$(powershell.exe -c 'Write-Host -NoNewLine  $env:userprofile' | xargs -0 wslpath)
+cd ${winhome}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
+sed -i '/"defaults"/,/fontFace/s/^\(.*fontFace": \).*/\1"CaskaydiaCove Nerd Font",/' settings.json
+sed -i '/"defaults"/,/fontSize/s/\([^/]*fontSize": \)[0-9]*/\115/' settings.json
+
+# .gitconfig settings
+copy "${WinGetConfigRoot}\..\.gitconfig" "~\.gitconfig" -Force
